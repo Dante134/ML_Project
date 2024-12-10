06124c6e6b7fa40be031e370f492030877d4935d
Ayush Mishra <f20201993@pilani.bits-pilani.ac.in>
2024-12-11 02:07:58 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPEN_E_DOT = '-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -17,11 +17,11 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
-version='0.0.1',
-author='Ayush',
-author_email='f20201993@pilani.bits-pilani.ac.in',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+name = 'mlproject',
+version = '0.0.1',
+author = 'Ayush',
+author_email = 'f20201993@pilani.bits-pilani.ac.in',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
 
 )
